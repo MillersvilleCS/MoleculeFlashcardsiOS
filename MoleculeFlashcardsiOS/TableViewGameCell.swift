06fd7fe37804281dce405b27c3d0023dfb5e2c35
kbroskie <kbroskie@gmail.com>
2014-07-01 13:25:30 -0400

    added custom cell

---
+++ MoleculeFlashcardsiOS/TableViewGameCell.swift
@@ -0,0 +1,17 @@
+//
+//  TableViewGameCell.swift
+//  MoleculeFlashcardsiOS
+//
+//  Created by exscitech on 7/1/14.
+//  Copyright (c) 2014 exscitech. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TableViewGameCell : UITableViewCell {
+    @IBOutlet var gameImage : UIImageView
+    @IBOutlet var gameButton : UILabel
+    
+    
+}
