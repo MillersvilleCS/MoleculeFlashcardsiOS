bec96140516b52e490a4eb23c0d23d69d709a831
kbroskie <kbroskie@gmail.com>
2014-07-01 11:57:07 -0400

    added controllers

---
+++ MoleculeFlashcardsiOS/CategoriesView.swift
@@ -0,0 +1,21 @@
+//
+//  CategoriesView.swift
+//  MoleculeFlashcardsiOS
+//
+//  Created by exscitech on 7/1/14.
+//  Copyright (c) 2014 exscitech. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CategoriesView : UIViewController {
+    
+    @IBOutlet var tableView : UITableView
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var categories: String[] = ["One", "Two", "Three"]
+    }
+}
---
+++ MoleculeFlashcardsiOS/GamesView.swift
@@ -0,0 +1,16 @@
+//
+//  GamesView.swift
+//  MoleculeFlashcardsiOS
+//
+//  Created by exscitech on 7/1/14.
+//  Copyright (c) 2014 exscitech. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class GamesView : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
