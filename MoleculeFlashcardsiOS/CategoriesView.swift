716a71eafafc44472653957b404918f0cdfc13d7
kbroskie <kbroskie@gmail.com>
2014-07-01 12:53:12 -0400

    fixed tabla veeew

--- MoleculeFlashcardsiOS/CategoriesView.swift
+++ MoleculeFlashcardsiOS/CategoriesView.swift
@@ -9,13 +9,41 @@
 import Foundation
 import UIKit
 
-class CategoriesView : UIViewController {
+class CategoriesView : UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet var tableView : UITableView
+    var categories: String[] = ["One", "Two", "Three"]
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var categories: String[] = ["One", "Two", "Three"]
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+
+    override func prepareForSegue (segue: UIStoryboardSegue!, sender: AnyObject!) {
+        if segue.identifier == "Load View" {
+            
+        // pass data to the next view
+        
+        }
+    }
+    
+    func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!) {
+        
+    }
+
+    // Set the number of cells
+    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+        return self.categories.count;
+    }
+    
+    // Create the cells
+    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+        var cell:UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
+        
+        cell.textLabel.text = self.categories[indexPath.row]
+        
+        return cell
     }
 }
--- MoleculeFlashcardsiOS/GamesView.swift
+++ MoleculeFlashcardsiOS/GamesView.swift
@@ -9,8 +9,44 @@
 import Foundation
 import UIKit
 
-class GamesView : UIViewController {
+class GamesView : UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    
+    @IBOutlet var tableView : UITableView
+    
+    var games: String[] = ["One", "Two", "Three"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
+    
+    override func prepareForSegue (segue: UIStoryboardSegue!, sender: AnyObject!) {
+        if segue.identifier == "Load View" {
+            
+            // Pass data to the next view
+            
+        }
+    }
+    
+    func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!) {
+        
+    }
+    
+    // Set the number of cells
+    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+        return self.games.count;
+    }
+    
+    // Create the cells
+    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+        var cell:UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
+        
+        cell.textLabel.text = self.games[indexPath.row]
+        
+        return cell
+    }
+
 }
