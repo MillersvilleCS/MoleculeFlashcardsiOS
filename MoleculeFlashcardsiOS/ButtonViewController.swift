a9fd5e2aed5b173f617fd3e674f8ca45ce7f84ad
kbroskie <kbroskie@gmail.com>
2014-07-28 14:38:26 -0400

    Only one button can now be selected at a time now.

--- MoleculeFlashcardsiOS/ButtonViewController.swift
+++ MoleculeFlashcardsiOS/ButtonViewController.swift
@@ -25,6 +25,7 @@ class ButtonViewController: UIViewController {
         super.viewDidLoad()
         buttons = [button1!, button2!, button3!, button4!, button5!, button6!]
         createButtons()
+        view.exclusiveTouch = true
     }
     
     override func didReceiveMemoryWarning() {
